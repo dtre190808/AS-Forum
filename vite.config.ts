90a7cb866c77e4d57eaa218353dea7ba32686c5e
dtre190808 <dtre1908@gmail.com>
2026-04-27 14:08:53 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/forum-as/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
